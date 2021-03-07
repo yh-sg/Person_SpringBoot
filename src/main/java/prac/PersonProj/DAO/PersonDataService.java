05ebad361a185ca0e178dc2f77bf3f64f9fddd17
yonghan <wangyh.92@gmail.com>
2021-03-07 14:53:15 +0800

    halfway

--- src/main/java/prac/PersonProj/DAO/PersonDataService.java
+++ src/main/java/prac/PersonProj/DAO/PersonDataService.java
@@ -4,34 +4,54 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import prac.PersonProj.Model.Person;
 
 @Repository("postgres")
 public class PersonDataService implements PersonDao {
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	public PersonDataService(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
 	@Override
 	public List<Person> selectAllPeople() {
-		return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+		final String sql = "SELECT id, name FROM person";
+		return jdbcTemplate.query(sql, (resultSet, i)->{
+			UUID id = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			return new Person(id, name);
+		});
 	}
 
 	@Override
 	public Optional<Person> selectPersonById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		final String sql = "SELECT id, name FROM person WHERE id = ?";
+		Person person = jdbcTemplate.queryForObject(
+				sql, 
+				new Object[]{id}, 
+				(resultSet, i)->{
+			UUID personId = UUID.fromString(resultSet.getString("id"));
+			String name = resultSet.getString("name");
+			return new Person(personId, name);
+		});
+		return Optional.ofNullable(person);
 	}
 
 	@Override
 	public int insertPerson(UUID id, Person person) {
-		// TODO Auto-generated method stub
-		return 0;
+		final String sql = "INSERT INTO person(id, name) VALUES (?, ?)";
+		return jdbcTemplate.update(sql, id, person.getName());
 	}
 
 	@Override
 	public int deletePersonById(UUID id) {
-		// TODO Auto-generated method stub
-		return 0;
+		final String sql = "DELETE * FROM person WHERE id = ?";
+		return jdbcTemplate.update(sql, id);
 	}
 
 	@Override
